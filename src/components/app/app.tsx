c72690a2e5e2d8c60fbd38702588508f1ae1acce
Your Name <you@example.com>
2025-03-07 08:27:10 +0200

    начало роутинга

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,40 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import MainPage from '../../pages/main-page/main-page';
 import ProductPage from '../../pages/product-page/product-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import BasketPage from '../../pages/basket-page/basket-page';
+
+import { AppRoute } from '../../const/app-route';
 
 
 function App (): JSX.Element {
   return (
-    <ProductPage/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage/>}
+        >
+        </Route>
+        <Route
+          path={`${AppRoute.Product}/:id`}
+          element={<ProductPage/>}
+        >
+        </Route>
+        <Route
+          path={AppRoute.Basket}
+          element={<BasketPage/>}
+        >
+        </Route>
+        <Route
+          path={AppRoute.NotFound}
+          element={<NotFoundPage/>}
+        >
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
