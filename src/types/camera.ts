31af1696cd6b47c156ba42e3e39ddbaa542acdfd
Your Name <you@example.com>
2025-03-15 11:52:41 +0700

    добавление типа для отзыва

--- src/types/camera.ts
+++ src/types/camera.ts
@@ -15,6 +15,19 @@ export type Camera = {
   reviewCount: number;
 }
 
+type Review = {
+  id:string;
+  userName: string;
+  advantage: string;
+  disadvantage: string;
+  review: string;
+  rating: number;
+  createAt: string;
+  cameraId: number;
+}
+
+export type Reviews = Review[];
+
 export type Cameras = Camera[];
 
 export type Promo = Pick<Camera, 'id'| 'name'| 'previewImg'| 'previewImg2x'| 'previewImgWebp'| 'previewImgWebp2x'>;
