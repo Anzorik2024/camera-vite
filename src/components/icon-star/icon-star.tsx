e6e58d46d6ece9a291a8e38f66e5f1d1e091ab9a
Your Name <you@example.com>
2025-03-23 23:24:51 +0700

    перед телефоном

--- src/components/icon-star/icon-star.tsx
+++ src/components/icon-star/icon-star.tsx
@@ -3,8 +3,8 @@ type IconStarProps = {
 }
 function IconStar({isFull}: IconStarProps): JSX.Element {
   return (
-    <svg width={17} height={16} aria-hidden="true">
-      <use xlinkHref={isFull ? '#icon-full-star' : '#icon-star'}/>
+    <svg width={17} height={16} aria-hidden="true" data-testid="img-icon">
+      <use xlinkHref={isFull ? '#icon-full-star' : '#icon-star'} data-testid="icon-use"/>
     </svg>
   );
 }
